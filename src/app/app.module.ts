9bc615ebe18c556b34828531175f3fe6665ed2df
Sanjana-027 <sanjanasingh02710@gmail.com>
2023-11-13 16:52:37 +0530

    Dashboard-Created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductCardComponent } from './shared/product-card/product-card.component';
-import { SidenavComponent } from './shared/sidenav/sidenav.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SidenavComponent } from './shared/sidenav/sidenav.component';
+import { ProductCardComponent } from './shared/product-card/product-card.component';
+import { SidePanelComponent } from './shared/side-panel/side-panel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductCardComponent,
+    DashboardComponent,
     SidenavComponent,
-    DashboardComponent
+    ProductCardComponent,
+    SidePanelComponent
   ],
   imports: [
     BrowserModule,

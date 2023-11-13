10f161e06d51067bc0b0c950df7b2853d973afc4
Sanjana-027 <sanjanasingh02710@gmail.com>
2023-11-13 12:55:48 +0530

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductCardComponent } from './shared/product-card/product-card.component';
+import { SidenavComponent } from './shared/sidenav/sidenav.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductCardComponent,
+    SidenavComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
